acfeb5b76b90e628928fac45bec5304f27ef6270
wlsnobug <wlansen@163.com>
2023-12-26 09:09:16 +0800

    提交项目所有代码“
    ；
    git commit -m 提交项目所有代码“

---
+++ include/server/model/friendmodel.hpp
@@ -0,0 +1,21 @@
+#ifndef FRIENDMODEL_H
+#define FRIENDMODEL_H
+
+#include <vector>
+#include "user.hpp"
+using namespace std;
+
+//维护好友信息的操作接口方法
+class FriendModel
+{
+public:
+    //添加好友关系
+    void insert(int userid,int friendid);
+
+    //返回用户好友列表
+    vector<User> qurey(int userid);
+};
+
+
+
+#endif
---
+++ include/server/model/groupuser.hpp
@@ -0,0 +1,16 @@
+#ifndef GROUPUSER_H
+#define GROUPUSER_H
+
+#include "user.hpp"
+// 群组用户，多了一个role角色信息，其余从User继承
+class GroupUser : public User
+{
+public:
+    void setRole(string role) { this->role = role; }
+    string getRole() { return this->role; }
+
+private:
+    string role;
+};
+
+#endif
